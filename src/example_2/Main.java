8406b7e2cef9237ce3160c5985401c44ad35c26f
Name <damiros.20002@gmail.com>
2023-05-13 14:04:03 +0300

    Выстраиваем структуру

---
+++ src/example_2/Main.java
@@ -0,0 +1,11 @@
+package example_2;
+
+import example_2.Application;
+
+public class Main {
+    static int count;
+    public static void main(String[] args){
+        count = 0;
+        Application app = new Application();
+    }
+}
